a810b004ade815163aa7d896e13f010de4d5e605
Trevor Njeru <tnjeru16@gmail.com>
2020-11-12 20:47:33 -0500

    debugged record_transfer method

--- portfolio.py
+++ portfolio.py
@@ -10,11 +10,12 @@ class Portfolio:
         self.age = currYear - birthYear
         self.cash_balance = 0.00
         self.total_equity = 0.00
+        self.total_value = 0.00
         self.shares = []
         self.transfer_history = []
         self.transaction_history = []
 
-    def recordTransfer(self, transfer_type, _amount, _balance):
+    def record_transfer(self, transfer_type, _amount, _balance):
         """Records a transfer as a deposit or withdrawal and final balance.
 
         Args:
@@ -27,14 +28,14 @@ class Portfolio:
         """
 
         transfer = dict()
-        transfer['transaction'] = transfer_type
+        transfer['transfer_type'] = transfer_type
         transfer['amount'] = _amount
         transfer['balance'] = _balance
         transfer['timestamp'] = "11/12/2020"
 
         return transfer
 
-    def transferFunds(self, is_deposit, amount):
+    def transfer_funds(self, is_deposit, amount):
         """Transfers between user's trading account and bank account,
         and records transaction details.
 
@@ -45,11 +46,16 @@ class Portfolio:
 
         if (is_deposit):
             self.cash_balance += amount
-            self.transfer_history.append(
-                recordTransfer("Deposit", amount, self.cash_balance))
-            print('Change: +$', amount)
+            history = self.record_transfer(
+                "Deposit", amount, self.cash_balance)
+            self.transfer_history.append(history)
+            print('Change: $+', amount)
             print('New Cash Balance: $', self.cash_balance)
         else:
-            self.cash_balance -= amount
-            print('Change: -$', amount)
+            amount *= -1
+            self.cash_balance += amount
+            history = self.record_transfer(
+                "Withdrawal", amount, self.cash_balance)
+            self.transfer_history.append(history)
+            print('Change: $-', amount)
             print('New Cash Balance: $', self.cash_balance)
